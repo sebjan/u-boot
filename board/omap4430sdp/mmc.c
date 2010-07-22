47413c724e8f59c1bc8b42e560a705033bf8d207
Moiz Sonasath <m-sonasath@ti.com>
2010-07-22 09:43:12 -0500

    U-boot: omap4_dev: Create partition table for EMMC flash using fastboot
    This patch creates the required partition table for EMMC boot and to
    support eMMc flash using fastboot.
    
    The partition table is as shown:
    Partition-Name	Sector-start-address	Size
    -----------------------------------------------------------------------
    mbr			0x00			512B(0x200)
    xloader		0x100			384KB(0x60000)
    bootloader		0x400			384KB(0x60000)
    environment		0x700			128KB(0x20000)
    kernel		0x800			6.5MB(0x680000)
    misc		0x3C00			512B(0x200)
    system		0x3EC1			200MB(0xC800000)
    userdata		0x69E5B			32MB(0x2000000)
    cache		0x7D820			32MB(0x2000000)
    recovery		0x911E5			300MB(0x12C00000)
    
    Change-Id: I822dd9b6e4c78e86b15c0ba54905a921dcfebf85
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- board/omap4430sdp/mmc.c
+++ board/omap4430sdp/mmc.c
@@ -34,8 +34,13 @@ void board_mmc_init(void)
 #if (CONFIG_FASTBOOT)
 
 	/* Partitons on EMMC preasent on OMAP4SDP required for Fastboot*/
-	fastboot_ptentry ptn[3] = {
-		/* Leaving Sector0- Sector -256 [128KB]for MBR */
+	fastboot_ptentry ptn[10] = {
+		{
+			.name   = "mbr",
+			.start  = 0x00, /*Sector Start */
+			.length = 0x200, /*512B */
+			.flags  = 0,
+		},
 		{
 			.name   = "xloader",
 			.start  = 0x100, /*Sector Start */
@@ -54,10 +59,46 @@ void board_mmc_init(void)
 			.length = 0x0020000, /*128KB */
 			.flags  = FASTBOOT_PTENTRY_FLAGS_WRITE_ENV,
 		},
-		/* Rest of the Partitions can start from Sector start 2048 */
+		{
+			.name   = "kernel",
+			.start  = 0x800,  /* Sector Start */
+			.length = 0x00680000, /*6.5MB */
+			.flags  = 0,
+		},
+		{
+			.name   = "misc",
+			.start  = 0x3C00, /*Sector Start */
+			.length = 0x200, /*512B */
+			.flags  = 0,
+		},
+		{
+			.name   = "system",
+			.start  = 0x3EC1,  /* Sector Start */
+			.length = 0xC800000, /*200MB */
+			.flags  = 0,
+		},
+		{
+			.name   = "userdata",
+			.start  = 0x69E5B,  /* Sector Start */
+			.length = 0x2000000, /*32MB */
+			.flags  = 0,
+		},
+		{
+			.name   = "cache",
+			.start  = 0x7D820,  /* Sector Start */
+			.length = 0x2000000, /*32MB */
+			.flags  = 0,
+		},
+		{
+			.name   = "recovery",
+			.start  = 0x911E5, /*Sector Start */
+			.length = 0x12C00000, /*300MB */
+			.flags  = 0,
+		},
+		/* Rest of the RAW Partitions can start from Sector start 0x1271E5 */
 	};
 	int i;
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 10; i++)
 		fastboot_flash_add_ptn(&ptn[i]);
 #endif
 }
