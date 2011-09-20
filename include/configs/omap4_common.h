230caa0f5ff8ebac513e11b4b9072221ca050570
John Rigby <john.rigby@linaro.org>
2011-09-19 23:02:52 -0600

    PXE: OMAP4: add standard env vars
    Set standard env vars in default environment.  These are currently
    only used by PXE which is only enabled on Panda but they are safe
    on all OMAP4 platforms.
    
    Signed-off-by: John Rigby <john.rigby@linaro.org>

--- include/configs/omap4_common.h
+++ include/configs/omap4_common.h
@@ -162,6 +162,10 @@
 	"mmcboot=echo Booting from mmc${mmcdev} ...; " \
 		"run mmcargs; " \
 		"bootm ${loadaddr}\0" \
+	"kernel_addr_r=0x88000000\0" \
+	"ramdisk_addr_r=0x81600000\0" \
+	"fdt_addr=0x87000000\0" \
+	"pxefile_addr_r=0x86000000\0"
 
 #define CONFIG_BOOTCOMMAND \
 	"if mmc rescan ${mmcdev}; then " \
